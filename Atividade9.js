aa721f967ec5cf0d01b74d9da070f95109503c98
Jadson Danilo <Jadson17danilo@gmail.com>
2024-10-01 21:47:56 +0000

    Atividade9.1

--- Atividade9.js
+++ Atividade9.js
@@ -4,3 +4,18 @@
 //candidato
 
 const prompt = require("prompt-sync")()
+
+const notaConcurso = parseFloat(prompt(`Digite a nota: `))
+
+if (notaConcurso <= 3) {
+    console.log(`tente no próximo ano!`)
+}
+else if (notaConcurso <= 5 ) {
+    console.log(`estude mais um pouco`)
+}
+else if (notaConcurso <= 7) {
+    console.log(`Parabéns! passou se arrastando`)
+}
+else {
+    console.log(`Parabens! fez o básico`)
+}
